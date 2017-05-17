e3e7886c4960e6a0391a2a8282f3be59255cf111
leadd <ddeal812@gmail.com>
2017-05-16 22:28:41 -0500

    Create AJAX request for NYT articles

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    //STREETVIEW
 
     //Create location string to feed to streetview API
     var preLoc = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=';
@@ -29,6 +29,38 @@ function loadData() {
     //Add or update image source value & greeting
     $('.bgimg').attr('src', preLoc + address);
     $greeting.text("So, you want to live at " + address + '?');
+
+    //NYT
+
+    //Update section header with user's chosen city
+    $nytHeaderElem.text('New York Times Articles about ' + $('#city').val());
+
+    //Create URL to use for NYT API
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "42ad017c5fbe42dd9aa999eee93786bd"
+    });
+
+    //Fire AJAX request for articles
+    $.getJSON(url)
+        .done(function(data) {
+            //Variable to hold the response location for articles
+            var articles = data.response.docs;
+
+            //Iterate over articles
+            for(var i = 0; articles.length > i; i++) {
+                //Create list item for each article in array
+                var article = document.createElement('li');
+
+                //Configure each article with class and content from array
+                article.innerHTML = articles[i].lead_paragraph;
+                article.classList.add('article');
+
+                //Append to the unordered list node
+                document.getElementById('nytimes-articles').appendChild(article);
+            }
+        });
+
 
     return false;
 };
